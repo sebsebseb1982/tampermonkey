2008f34e0b69c92cc18673739fd717299f34d592
sebsebseb1982 <lapoubelleamail@gmail.com>
2018-06-06 20:15:35 +0200

    Update gixen.js

--- gixen.js
+++ gixen.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gixen
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://www.gixen.com/home_2.php*
