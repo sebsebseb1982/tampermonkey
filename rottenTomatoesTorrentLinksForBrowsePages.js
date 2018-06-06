30bcadc07f065f0198f5b580fa5e02eed757fdb9
sebsebseb1982 <lapoubelleamail@gmail.com>
2018-06-07 00:18:41 +0200

    Update rottenTomatoesTorrentLinksForBrowsePages.js

--- rottenTomatoesTorrentLinksForBrowsePages.js
+++ rottenTomatoesTorrentLinksForBrowsePages.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rotten Tomatoes Torrent Links (Browse pages)
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.rottentomatoes.com/browse/*
