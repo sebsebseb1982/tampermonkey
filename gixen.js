ebef5c3635646e88ea3d3fa7e5f8112df61ec735
sebsebseb1982 <lapoubelleamail@gmail.com>
2021-09-06 16:56:30 +0200

    Update gixen.js

--- gixen.js
+++ gixen.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  Gixen auto login
 // @author       You
-// @match        https://www.gixen.com/home_2.php*
+// @match        https://www.gixen.com/main/home_2.php*
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        none
