476d4956dce8e20330afbbb7e81e3a88d90c8dd3
sebsebseb1982 <lapoubelleamail@gmail.com>
2023-10-16 20:04:44 +0200

    Create youtube-to-invidious.js

---
+++ youtube-to-invidious.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         YouTube to Invidious
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        *://www.youtube.com/watch?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    if (window.location.href.includes("youtube.com")) {
+        let videoID = new URLSearchParams(window.location.search).get('v');
+        if (videoID) {
+            window.location.replace(`https://invidious.fdn.fr/watch?v=${videoID}`);
+        }
+    }
+})();
